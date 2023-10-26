97100b737af781e77fcc9fedfdbcd64e97032f66
Lakshya Gadhwal <118717979+DarthVishnu8@users.noreply.github.com>
2023-10-26 13:40:35 +0000

    somewhat works. atleast splitting does

--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -4,6 +4,73 @@
 #include<unordered_map>
 
 using namespace std;
+
+// Murmur2 hash function
+size_t _Hash_bytes(const char* buf, size_t len, size_t seed) {
+    const size_t m = 0x5bd1e995;
+    size_t hash = seed ^ len;
+    while (len >= 4) {
+        size_t k = *((const size_t*)buf);
+        k *= m; k ^= k >> 24; k *= m;
+        hash *= m; hash ^= k;
+        buf += 4; len -= 4;
+    }
+    size_t k;
+    switch (len) {
+        case 3: k = buf[2]; hash ^= k << 16;
+        case 2: k = buf[1]; hash ^= k << 8;
+        case 1: k = buf[0]; hash ^= k; hash *= m;
+    }
+    hash ^= hash >> 13; hash *= m; hash ^= hash >> 15;
+    return hash;
+}
+
+class MyMap {
+public:
+    MyMap() : buckets(100) {} // Choose an initial size for the hash table.
+
+
+    int& operator[](const std::string& key) {
+        size_t hash = _Hash_bytes(key.c_str(), key.length(), 0);
+        size_t index = hash % buckets.size();
+        for (auto& pair : buckets[index]) {
+            if (pair.first == key) {
+                return pair.second;
+            }
+        }
+        buckets[index].emplace_back(key, 0);
+        return buckets[index].back().second;
+    }
+    // Insert a key-value pair into the map.
+    void insert(const std::string& key, int value) {
+        size_t hash = _Hash_bytes(key.c_str(), key.length(), 0);
+        size_t index = hash % buckets.size();
+        for (auto& pair : buckets[index]) {
+            if (pair.first == key) {
+                // Key already exists; update the value.
+                pair.second = value;
+                return;
+            }
+        }
+        buckets[index].emplace_back(key, value);
+    }
+
+    // Search for a key and return its value.
+    int find(const std::string& key) {
+        size_t hash = _Hash_bytes(key.c_str(), key.length(), 0);
+        size_t index = hash % buckets.size();
+        for (const auto& pair : buckets[index]) {
+            if (pair.first == key) {
+                return pair.second;
+            }
+        }
+        return -1; // Key not found.
+    }
+
+private:
+    std::vector<std::vector<std::pair<std::string, int>>> buckets;
+};
+
 vector<string> splitt(string s)
 {
     string tmp="";
@@ -20,6 +87,7 @@ vector<string> splitt(string s)
             tmp="";
         }
     }
+    ans.push_back(tmp);
     return ans;
 }
 
@@ -42,12 +110,14 @@ vector<string> splitn(string s)
     return ans;
 }
     
-    std::unordered_map<string,int> order_book;
 
-string processor(string stocking){
+string processor(string &stocking, MyMap &order_book){
     string ans="No Trade\n";
+    //cout << "yes im here";
     vector<string> order=splitt(stocking);
-    cout << order[0] << " "<< order[1] << " "<<order[2];
+    //cout<< "splitting done";
+    //cout << order[0] << order[1] <<order[2];
+    cout << order_book[order[0]];
     if(order_book[order[0]]==0)
     {
         order_book[order[0]]=stoi(order[1]);
@@ -68,14 +138,22 @@ string processor(string stocking){
 }
 int main() {
 
+    MyMap order_book;
     Receiver rcv;
-    sleep(4);
+    // sleep(4);
     std::string message = rcv.readIML();
+    //cout<<message;
     vector<string> ans;
     ans = splitn(message);
-    for(auto c:ans){
-        cout << processor(c);
+    int s= ans.size();
+    for(int i=0; i<s;i++){
+        cout << processor(ans[i],order_book);
     }
+    //cout << processor(ans[0],order_book);
+    // vector<string> s1 = splitt(ans[0]);
+    // for (auto c: s1){
+    //     cout <<"splitline"<< c;
+    // }
     // std::string line;
     // cout <<R
     // size_t start = 0;
