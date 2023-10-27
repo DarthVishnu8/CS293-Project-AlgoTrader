0043cbb3caf34ba7fdcd3e8530a5765dc9c5019a
Ankeet1729 <112777660+Ankeet1729@users.noreply.github.com>
2023-10-27 21:12:51 +0530

    unfucked laskhya's fuck up

--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -1,74 +1,64 @@
 #include "receiver.h"
 #include <vector>
 #include <string>
-#include<unordered_map>
 
 using namespace std;
 
-// Murmur2 hash function
-size_t _Hash_bytes(const char* buf, size_t len, size_t seed) {
-    const size_t m = 0x5bd1e995;
-    size_t hash = seed ^ len;
-    while (len >= 4) {
-        size_t k = *((const size_t*)buf);
-        k *= m; k ^= k >> 24; k *= m;
-        hash *= m; hash ^= k;
-        buf += 4; len -= 4;
-    }
-    size_t k;
-    switch (len) {
-        case 3: k = buf[2]; hash ^= k << 16;
-        case 2: k = buf[1]; hash ^= k << 8;
-        case 1: k = buf[0]; hash ^= k; hash *= m;
-    }
-    hash ^= hash >> 13; hash *= m; hash ^= hash >> 15;
-    return hash;
-}
-
-class MyMap {
-public:
-    MyMap() : buckets(100) {} // Choose an initial size for the hash table.
-
-
-    int& operator[](const std::string& key) {
-        size_t hash = _Hash_bytes(key.c_str(), key.length(), 0);
-        size_t index = hash % buckets.size();
-        for (auto& pair : buckets[index]) {
-            if (pair.first == key) {
-                return pair.second;
-            }
-        }
-        buckets[index].emplace_back(key, 0);
-        return buckets[index].back().second;
-    }
-    // Insert a key-value pair into the map.
-    void insert(const std::string& key, int value) {
-        size_t hash = _Hash_bytes(key.c_str(), key.length(), 0);
-        size_t index = hash % buckets.size();
-        for (auto& pair : buckets[index]) {
-            if (pair.first == key) {
-                // Key already exists; update the value.
-                pair.second = value;
-                return;
-            }
-        }
-        buckets[index].emplace_back(key, value);
-    }
-
-    // Search for a key and return its value.
-    int find(const std::string& key) {
-        size_t hash = _Hash_bytes(key.c_str(), key.length(), 0);
-        size_t index = hash % buckets.size();
-        for (const auto& pair : buckets[index]) {
-            if (pair.first == key) {
-                return pair.second;
-            }
-        }
-        return -1; // Key not found.
-    }
-
+template <typename KeyType, typename ValueType>
+class MyUnorderedMap {
 private:
-    std::vector<std::vector<std::pair<std::string, int>>> buckets;
+    struct Entry {
+        KeyType key;
+        ValueType value;
+        bool occupied;
+
+        Entry() : occupied(false) {}
+    };
+
+    std::vector<Entry> table;
+
+    size_t hash(const KeyType& key) {
+        return std::hash<KeyType>{}(key) % table.size();
+    }
+
+    size_t findEmptySlot(const KeyType& key) {
+        size_t index = hash(key);
+        while (table[index].occupied) {
+            if (table[index].key == key) {
+                return index;
+            }
+            index = (index + 1) % table.size(); // Linear probing
+        }
+        return index;
+    }
+
+    void resize() {
+        // This is a simplified implementation, and we are not resizing the table here.
+        // A production-quality unordered map would resize the table to maintain a low load factor.
+    }
+
+public:
+    MyUnorderedMap(size_t initialSize = 100) {
+        table.resize(initialSize);
+    }
+
+    void insert(const KeyType& key, const ValueType& value) {
+        resize();
+        size_t index = findEmptySlot(key);
+        table[index].key = key;
+        table[index].value = value;
+        table[index].occupied = true;
+    }
+
+    ValueType& operator[](const KeyType& key) {
+        size_t index = findEmptySlot(key);
+        if (!table[index].occupied) {
+            table[index].key = key;
+            table[index].value = ValueType();
+            table[index].occupied = true;
+        }
+        return table[index].value;
+    }
 };
 
 vector<string> splitt(string s)
@@ -115,40 +105,40 @@ vector<string> splitn(string s)
 }
     
 
-string processor(string &stocking, MyMap &order_book){
-    string ans="No Trade\n";
+string processor(string &stocking, MyUnorderedMap<string,int> &order_book){
+    string ans="No Trade\r";
     //cout << "yes im here";
     vector<string> order=splitt(stocking);
     //return "_"+order[0]+"*";
     
     //cout<< "splitting done";
-    //cout <<"\n test:"<< order[0] <<" "<< order[1] <<order[2];
+    //cout <<"\r test:"<< order[0] <<" "<< order[1] <<order[2];
     //cout << order_book[order[0]];
     if(order_book[order[0]]==0)
     {
-        //cout <<"order 1 is:"<< order[1]<<"\n";
+        //cout <<"order 1 is:"<< order[1]<<"\r";
         order_book[order[0]]=stoi(order[1]);
         //bool flag =(order[0]=="ASML");
         //cout <<"flag is"<< flag;
         //cout << " changed to :" <<order_book["ASML"];
         if(order[2] == "b"){
-            ans = order[0]+ " " + order[1]+ " s\n";
+            ans = order[0]+ " " + order[1]+ " s\r";
             return ans;
         }
         else{
-            ans = order[0]+ " " + order[1]+ " s\n";
+            ans = order[0]+ " " + order[1]+ " b\r";
             return ans;
         }
     }
     if(order[2] == "b"){
         if(stoi(order[1]) > order_book[order[0]]){
-            ans = order[0]+ " " + order[1]+ " s\n";
+            ans = order[0]+ " " + order[1]+ " s\r";
             order_book[order[0]]= stoi(order[1]);
             }
     }
     else {
         if(stoi(order[1]) < order_book[order[0]]){
-            ans = order[0]+ " " + order[1]+ " b\n";
+            ans = order[0]+ " " + order[1]+ " b\r";
             order_book[order[0]]= stoi(order[1]);
             }
     }
@@ -156,7 +146,7 @@ string processor(string &stocking, MyMap &order_book){
 }
 int main() {
 
-    MyMap order_book;
+    MyUnorderedMap<string,int> order_book;
     Receiver rcv;
     // sleep(4);
     std::string message = rcv.readIML();
@@ -170,30 +160,7 @@ int main() {
         //cout << "New value:" <<order_book["ASML"];
 
     }
-    //cout << processor(ans[0],order_book);
-    // vector<string> s1 = splitt(ans[0]);
-    // for (auto c: s1){
-    //     cout <<"splitline"<< c;
-    // }
-    // std::string line;
-    // cout <<R
-    // size_t start = 0;
-    // size_t end = message.find('\n');
-    // while (end != std::string::npos) {
-    //     line = message.substr(start, end - start);
-    //     lines.push_back(line);
-    //     start = end + 1;
-    //     end = message.find('\n', start);
-    // }
     
-    // // Add the last line (if any)
-    // line = message.substr(start, end);
-    // lines.push_back(line);
-    // cout<<lines.size()<<" is the size";
-    // // Now 'lines' contains the individual lines
-    // for (const std::string& s : lines) {
-    //     std::cout << s <<"is it done"<< std::endl;
-    // }
     
     return 0;
 }
