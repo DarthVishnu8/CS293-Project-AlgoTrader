8e187d779bb899dc57ea04e28be20eee48cb0c3b
Lakshya Gadhwal <118717979+DarthVishnu8@users.noreply.github.com>
2023-11-04 07:40:14 +0000

    Working code
    Code logic corrected

--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -67,8 +67,7 @@ vector<string> splitt(string s)
     vector<string> ans;
     for(char& c: s)
     {
-    if(int(c) == 13){//cout<<"CARRIGE RETURN SAALA";
-    continue;}
+    if(int(c) == 13) continue;
         else if(c!=' ')
         {
             tmp+=c;
@@ -90,11 +89,7 @@ vector<string> splitn(string s)
     vector<string> ans;
     for(auto c: s)
     {
-        if(c!='#')
-        {
-            tmp+=c;
-        }
-        //else if(c == '\r'){cout<<"chutiya";continue;}
+        if(c!='#') tmp+=c;
         else
         {
             ans.push_back(tmp);
@@ -105,93 +100,126 @@ vector<string> splitn(string s)
 }
     
 
-string processor(string &stocking, MyUnorderedMap<string,int> &order_book){
+string processor(string &stocking, MyUnorderedMap<string,std::vector<int>> &order_book){
     string ans="No Trade\r";
-    //cout << "yes im here";
     vector<string> order=splitt(stocking);
-    //return "_"+order[0]+"*";
-    
-    //cout<< "splitting done";
-    //cout <<"\r test:"<< order[0] <<" "<< order[1] <<order[2];
-    //cout << order_book[order[0]];
-    if(order_book[order[0]]==0)
+
+    string stock = order[0];
+    int bid = stoi(order[1]);
+
+    if(order_book[stock].size()==0)
     {
-        //cout <<"order 1 is:"<< order[1]<<endl;
-        order_book[order[0]]=stoi(order[1]);
-        //bool flag =(order[0]=="ASML");
-        //cout <<"flag is"<< flag;
-        //cout << " changed to :" <<order_book["ASML"];
+        order_book[stock].push_back(0);
+        order_book[stock].push_back(0);
+        order_book[stock].push_back(bid);
+
         if(order[2] == "b"){
             ans = order[0]+ " " + order[1]+ " s\r";
+            //ans = ans+"new trade buy";
             return ans;
         }
+
         else{
             ans = order[0]+ " " + order[1]+ " b\r";
+            //ans+="new trade sell";
             return ans;
         }
+
     }
+
     if(order[2] == "b"){
-        if(stoi(order[1]) > order_book[order[0]]){
+
+        if(order_book[stock][0] != 0 && bid <= order_book[stock][0]){
+        // ans+="dead by skill issue";
+            return ans;
+        } // bad buy order dies instantly
+
+        if(order_book[stock][1] != 0 && bid == order_book[stock][1]) {       // equal (alive) buy and sell orders cancel each other
+            order_book[stock][0] = 0; 
+            order_book[stock][1] = 0;
+            // ans+="dead by ko";
+            return ans;
+            }
+
+        if(bid > order_book[stock][2]){
             ans = order[0]+ " " + order[1]+ " s\r";
-            order_book[order[0]]= stoi(order[1]);
+            order_book[stock][2]= bid;
+            order_book[stock][0]= 0;
+            // ans+="sold";
+            return ans;
             }
+        else{
+            order_book[stock][0] = bid;
+            // ans+="bad luck";
+            return ans;
+        }
+
     }
+
     else {
-        if(stoi(order[1]) < order_book[order[0]]){
-            ans = order[0]+ " " + order[1]+ " b\r";
-            order_book[order[0]]= stoi(order[1]);
+
+        if(order_book[stock][1] != 0 && bid >= order_book[stock][1]){
+        // ans+="dead by skill issue";
+            return ans;
+        } // bad sell order dies instantly
+
+        if(order_book[stock][0] != 0 && bid == order_book[stock][0]) {       // equal (alive) buy and sell orders cancel each other
+            order_book[stock][0] = 0; 
+            order_book[stock][1] = 0;
+            // ans+="dead by ko";
+            return ans;
             }
+
+        if(bid < order_book[stock][2]){
+            ans = order[0]+ " " + order[1]+ " b\r";
+            order_book[stock][2]= bid;
+            order_book[stock][1]= 0;
+            // ans+="bought";
+            return ans;
+            }
+        else{
+            order_book[stock][1] = bid;
+            // ans+="bad luck";
+            return ans;
+        }
+
     }
+
     return ans;
 }
 int main(int argc, char **argv) {
 
     if(strcmp(argv[1],"1")==0){
-    MyUnorderedMap<string,int> order_book;
+    MyUnorderedMap<string,vector<int>> order_book;
     Receiver rcv;
     // sleep(4);
     std::string message = rcv.readIML();
     //cout<<message;
-    bool characterExists = 0;
 
+
+    bool characterExists = 0;
 
     while(!characterExists){
     characterExists = message.find('$') != std::string::npos;
     vector<string> ans;
-    //while(message != "$"){
     ans = splitn(message);
     int s= ans.size();
-    //cout << "been here";
+    
     for(int i=0; i<s;i++){
         cout << processor(ans[i],order_book);
         // cout << ans[i];
-        //cout << int(ans[i][0]);
-        //cout << "New value:" <<order_book["ASML"];
-
     }
-    //cout  << message;
+
     if(!characterExists) message = rcv.readIML();
     else break;
+
     }
     }
+
+
     else if(strcmp(argv[1],"2")==0){
         cout<<"argument is 2";
     }
-    //cout<<message << "second time";
-    //ans.clear();
-    
-    //cout<<characterExists;
-    
-    //   if(message[0] == '$'){cout << "end";}
-    //}
-    // ans = splitn(message);
-    // s= ans.size();
-    // for(int i=0; i<s;i++){
-    //     cout << processor(ans[i],order_book);
-    //     //cout << int(ans[i][0]);
-    //     //cout << "New value:" <<order_book["ASML"];
-
-    // }
     
     return 0;
 }
