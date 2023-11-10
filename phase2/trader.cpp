afe0776156bc011cf794576336e9aa1c78714bb7
Ankeet1729 <112777660+Ankeet1729@users.noreply.github.com>
2023-11-10 22:06:21 +0530

    Update trader.cpp

--- phase2/trader.cpp
+++ phase2/trader.cpp
@@ -1,14 +1,52 @@
 // This is your trader. Place your orders from here
 
 
-#include <string>
+// #include <string>
 
-int reader(int time)
+// int reader(int time)
+// {
+//     return 1;
+// }
+
+// int trader(std::string *message)
+// {
+//     return 1;
+// }
+
+// above this is the original code
+
+// This is your trader. Place your orders from here
+
+
+#include <string>
+#include <vector>
+#include <fstream>
+    
+std::vector<std::string> orderList;    // declaring global variable orderList here
+
+int reader(int time)                   // this part of the code reads the required output file (a test output file named myoutput.txt for now) and stores every line in the form a vector<string> named orderList
 {
+    std::ifstream inputFile;
+    inputFile.open("myoutput.txt");
+
+
+    // Read from the file
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        orderList.push_back(line);
+    }
+
+    // Close the file
+    inputFile.close();
+
+    // return 0; // Return success
     return 1;
+
 }
 
 int trader(std::string *message)
 {
+    
     return 1;
 }
+
